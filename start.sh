df59e05a93ec30b98631a2695da0dd4f78736b53
Jos Hendriks <jos@hendriks.dev>
2023-02-19 14:34:21 +0100

    Fixed passwordhash parameter

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ! -z "${JUPYTER_PASSWORD_HASH}" ]]; then
-  jupyter lab --NotebookApp.token='' --NotebookApp.password='${JUPYTER_PASSWORD_HASH}'
+  jupyter lab --NotebookApp.token='' --NotebookApp.password=${JUPYTER_PASSWORD_HASH}
 elif [[ "${JUPYTER_NO_PASSWORD}" == "true" ]]; then
   jupyter lab --NotebookApp.token='' --NotebookApp.password=''
 else
